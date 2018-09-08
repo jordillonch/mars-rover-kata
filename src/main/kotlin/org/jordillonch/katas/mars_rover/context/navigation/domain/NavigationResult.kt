b171cd5a84bb1b44cdb0cd3ff3aa7a3131364b94
Jordi Llonch <llonch.jordi@gmail.com>
2018-09-08 21:51:18 +0200

    initial model

--- src/main/kotlin/org/jordillonch/katas/mars_rover/context/navigation/domain/NavigationResult.kt
+++ src/main/kotlin/org/jordillonch/katas/mars_rover/context/navigation/domain/NavigationResult.kt
@@ -1,6 +1,6 @@
 package org.jordillonch.katas.mars_rover.context.navigation.domain
 
-data class NavigationResponse(val status: Status, val commandsExecuted: List<Commands>)
+data class NavigationResult(val status: Status, val pose: Pose, val commandsExecuted: List<Commands>)
 
 enum class Status {
     SUCCESS
--- src/test/kotlin/org/jordillonch/katas/mars_rover/behaviour/NavigationRoversModuleTest.kt
+++ src/test/kotlin/org/jordillonch/katas/mars_rover/behaviour/NavigationRoversModuleTest.kt
@@ -3,18 +3,29 @@ package org.jordillonch.katas.mars_rover.behaviour
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.ShouldSpec
 import org.jordillonch.katas.mars_rover.context.navigation.domain.Commands.FORWARD
-import org.jordillonch.katas.mars_rover.context.navigation.domain.NavigationResponse
+import org.jordillonch.katas.mars_rover.context.navigation.domain.Direction.NORTH
+import org.jordillonch.katas.mars_rover.context.navigation.domain.MapSize
+import org.jordillonch.katas.mars_rover.context.navigation.domain.Mark
+import org.jordillonch.katas.mars_rover.context.navigation.domain.NavigationResult
 import org.jordillonch.katas.mars_rover.context.navigation.domain.NavigationRoversModule
-import org.jordillonch.katas.mars_rover.context.navigation.domain.Status
+import org.jordillonch.katas.mars_rover.context.navigation.domain.Position
+import org.jordillonch.katas.mars_rover.context.navigation.domain.Pose
+import org.jordillonch.katas.mars_rover.context.navigation.domain.Status.SUCCESS
+import org.jordillonch.katas.mars_rover.context.navigation.domain.TerritoryMarksMap
 
 class NavigationRoversModuleTest : ShouldSpec(
         {
-            val navigationRoversModule = NavigationRoversModule()
+            val marks = emptyMap<Position, Mark>()
+            val marsMap = TerritoryMarksMap(MapSize(Position(-100, 100),
+                                                    Position(100, -100)),
+                                            marks)
+            val initialPose = Pose(Position(0, 0), NORTH)
+            val navigationRoversModule = NavigationRoversModule(marsMap, initialPose)
 
             should("move forward 3 steps") {
                 val commands = listOf(FORWARD, FORWARD, FORWARD)
-                val response = navigationRoversModule.execute(commands)
+                val result = navigationRoversModule.execute(commands)
 
-                response.shouldBe(NavigationResponse(Status.SUCCESS, commands))
+                result.shouldBe(NavigationResult(SUCCESS, Pose(Position(0, 3), NORTH), commands))
             }
         })
